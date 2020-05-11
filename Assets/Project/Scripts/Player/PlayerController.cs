31855ae8df120a6a80addaef5c75451ca157c598
Jason Adams <jma@redwoodtech.com>
2020-05-11 14:49:23 +0100

    Player movement.

---
+++ Assets/Project/Scripts/Player/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed = 5f;
+    [SerializeField]
+    private Rigidbody2D rb = null;
+
+    private Vector2 movement;
+
+    private void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+}
